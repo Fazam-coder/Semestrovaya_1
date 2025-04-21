b047863c84481d94a7c3b4f4a73ec4dd47633aeb
Fazam-coder <amirrf06@gmail.com>
2025-04-21 22:23:34 +0300

    sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class MergeSort {
+    private static int[] a;
+
+    private static void merge(int l, int m, int r) {
+        int i=l, j=m+1, k=l;
+        int[] c = new int[a.length];
+        while (i <= m && j <= r)
+            if (a[i] < a[j]) {c[k]=a[i]; i++; k++;}
+            else {c[k]=a[j]; j++; k++;}
+        while (i<=m) {c[k]=a[i]; i++; k++;}
+        while (j<=r) {c[k]=a[j]; j++; k++;}
+        for (int n = l; n <= r; n++) {
+            a[n] = c[n];
+        }
+    }
+
+    public static void msort(int l, int r) {
+        if (l>=r) return;
+        int m = (l+r)/2;
+        msort(l, m);
+        msort(m+1, r);
+        merge(l, m, r);
+    }
+
+    public static void sort(int[] b) {
+        a = b;
+        msort(0, a.length - 1);
+    }
 }
--- TestSort.java
+++ TestSort.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class TestSort {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("data.txt"));
+        for (String line: lines) {
+            String[] items = line.split(" ");
+            int[] lst = new int[items.length];
+            for (int i = 0; i < lst.length; i++) {
+                lst[i] = Integer.valueOf(items[i]);
+            }
+
+            long start = System.nanoTime();
+            MergeSort.sort(lst);
+            long end = System.nanoTime();
+            System.out.println(lst.length + " " + (end - start));
+
+        }
+    }
 }
